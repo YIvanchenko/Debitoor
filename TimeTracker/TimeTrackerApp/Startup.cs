51ee5cf28065abb30bcfc4ffe232bcfbd4fd1272
Yuri Ivanchenko <yuri.ivanchenko.ua@gmail.com>
2015-09-02 01:04:14 +0200

    Initial commit.

---
+++ TimeTracker/TimeTrackerApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeTrackerApp.Startup))]
+namespace TimeTrackerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
