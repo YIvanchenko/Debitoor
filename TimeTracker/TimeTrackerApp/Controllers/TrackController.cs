00b69d9ee676c01ec74c1a71b88dcb6bbca775c9
Yuri Ivanchenko <yuri.ivanchenko.ua@gmail.com>
2015-09-04 02:52:41 +0200

    Overview and Time registration pages.

---
+++ TimeTracker/TimeTrackerApp/Controllers/TrackController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TimeTrackerApp.DataModels;
+using TimeTrackerApp.Models;
+
+namespace TimeTrackerApp.Controllers
+{
+    [Authorize]
+    public class TrackController : Controller
+    {
+        private TimesheetsContext _timesheetsStore = new TimesheetsContext();
+
+        // GET: Track
+        public ActionResult Time()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Time(TrackedTimeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var timesheet = Mapper.Map<Timesheet>(model);
+                    _timesheetsStore.Timesheets.Add(timesheet);
+                    _timesheetsStore.SaveChanges();
+
+                    return View("TimeConfirmation");
+                }
+                catch (DataException)
+                {
+                    ModelState.AddModelError("", "Could save your time.");
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        
+        public ActionResult Overview()
+        {
+            var timesheets = _timesheetsStore.Timesheets.OrderByDescending(t => t.Id);
+            var trackedTimeList = Mapper.Map<IEnumerable<TrackedTimeViewModel>>(timesheets);
+
+            return View(trackedTimeList);
+        }
+    }
+}
